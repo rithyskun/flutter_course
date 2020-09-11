e460dd41c9f40383656e54d2c824074131d6ccd8
Rithy SKUN <rithy.skun@outlook.com>
2020-09-11 20:50:24 +0700

    Finshed the QuizApp

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,14 +10,15 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  int _currentQuestionIndex = 0;
   List questionBank = [
     Question.name(
         "The U.S. Declaration of Independence was adopted in 1776.", true),
     Question.name("The Supreme law of the land is the Constitution.", true),
     Question.name(
         "The two rights in the Declaration of Independence are:"
-        "\n Life"
-        "\n Pursuit of happiness.",
+        "\n 1. Life"
+        "\n 2. Pursuit of happiness.",
         true),
     Question.name("The (U.S.) Contitution has 26 Amendments.", false)
   ];
@@ -28,41 +29,108 @@ class _QuizAppState extends State<QuizApp> {
         title: Text("True Citizen"),
         centerTitle: true,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                "images/flag.png",
-                width: 250,
-                height: 180,
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(14.4),
-                  border: Border.all(
-                      color: Colors.blueGrey.shade200,
-                      style: BorderStyle.solid)),
-              height: 120,
-              child: Center(
-                  child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  questionBank[1].questionText,
-                  style: TextStyle(fontSize: 16.9, color: Colors.black),
+      //we use [Builder] here to use a context that is descendent of Scaffold or else Scaffold.of return
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  "images/flag.png",
+                  width: 250,
+                  height: 180,
                 ),
-              )),
-            ),
-            Spacer(),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(14.0),
+                      border: Border.all(
+                          color: Colors.blueGrey.shade400,
+                          style: BorderStyle.solid)),
+                  height: 120,
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      questionBank[_currentQuestionIndex].questionText,
+                      style: TextStyle(fontSize: 16.9, color: Colors.black),
+                    ),
+                  )),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(true, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "TRUE",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(false, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "FALSE",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _nextQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  _checkAnswer(bool userChoice, BuildContext context) {
+    if (userChoice == questionBank[_currentQuestionIndex].isCorrect) {
+      final snackbar = SnackBar(
+          backgroundColor: Colors.green,
+          duration: Duration(microseconds: 50000),
+          content: Text("Yes, Correct"));
+      _updateQuestion();
+      Scaffold.of(context).showSnackBar(snackbar);
+      // debugPrint("Yes, Correct");
+    } else {
+      final snackbar = SnackBar(
+          backgroundColor: Colors.redAccent,
+          duration: Duration(microseconds: 50000),
+          content: Text("No, Incorrect"));
+      _updateQuestion();
+      Scaffold.of(context).showSnackBar(snackbar);
+      // debugPrint("No, not correct");
+    }
+  }
+
+  _updateQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % questionBank.length;
+    });
+  }
+
+  _nextQuestion() {
+    _updateQuestion();
+  }
 }
+
+// End QuizApp
 
 // BillSpitter
 class BillSplitter extends StatefulWidget {
